8df9d3e163a7c8bfcd1c3ada78d0ab17034ec1ce
sagip <sagip@192.168.0.6>
2019-11-16 20:27:52 +0530

    sys added

--- spring3-mvc-maven-xml-hello-world/src/main/java/com/mkyong/web/controller/HelloController.java
+++ spring3-mvc-maven-xml-hello-world/src/main/java/com/mkyong/web/controller/HelloController.java
@@ -14,6 +14,7 @@ public class HelloController {
 	public String printWelcome(ModelMap model) {
 
 		model.addAttribute("message", "Spring 3 MVC Hello World");
+		System.out.println("sdfsdfsdfsdfsd");
 		return "hello";
 
 	}
